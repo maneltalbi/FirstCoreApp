0f1339cd5c2d6788bbaf470e7003a756e5a901ed
maneltalbi <talbimanel20@gmail.com>
2022-02-03 18:05:37 +0100

    gestion des categories

---
+++ FirstCoreApp/Controllers/CategoriesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FirstCoreApp.Models;
+
+namespace FirstCoreApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly NewsDBContext db;
+
+        public CategoriesController(NewsDBContext context)
+        {
+            db = context;
+        }
+
+        // GET: Categories
+        public  IActionResult Index()
+        {
+            return View( db.Categories.ToList());
+        }
+
+        // GET: Categories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categories = await db.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categories == null)
+            {
+                return NotFound();
+            }
+
+            return View(categories);
+        }
+
+        // GET: Categories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Add(categories);
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categories);
+        }
+
+        // GET: Categories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categories = await db.Categories.FindAsync(id);
+            if (categories == null)
+            {
+                return NotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Categories categories)
+        {
+            if (id != categories.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Update(categories);
+                    await db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoriesExists(categories.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categories);
+        }
+
+        // GET: Categories/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categories =  db.Categories
+                .FirstOrDefault(m => m.Id == id);
+            if (categories == null)
+            {
+                return NotFound();
+            }
+
+            return View(categories);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var categories =  db.Categories.Find(id);
+            db.Categories.Remove(categories);
+             db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoriesExists(int id)
+        {
+            return db.Categories.Any(e => e.Id == id);
+        }
+    }
+}
